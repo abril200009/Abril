031debfd7c3a9c59b39fc4840253fbae382ef684
abril200009 <ap2405118@gmail.com>
2025-03-25 14:10:55 -0600

    Add files via upload

---
+++ 25_03_25 ab.cpp
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+int main() {
+    int arreglo[100];
+    int i = 0;
+    
+    while (i < 100){
+    arreglo[i] = 2 * (i + 1);
+    i++;
+    }
+    i = 0;
+    while(i < 100){
+    printf("2 * %d = %d\n",i + 1, arreglo[i]);
+    i++;
+    }
+    return  0;
+    
+    }
+   
