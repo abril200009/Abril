1bd91b5a03740574a477d3749547839499a3cd65
abril200009 <ap2405118@gmail.com>
2025-04-01 14:15:59 -0600

    Add files via upload

---
+++ strlen.cpp 010425.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+int len;
+char cad[]="Es una cadena";
+ len = strlen(cad);
+    
+    printf("la longitud de:\ '%s\' es: %d\n", cad, len);
+
+    return 0;
+}
