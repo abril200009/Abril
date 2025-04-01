98ffda91dda85031b081ae4822496c1693dd5275
abril200009 <ap2405118@gmail.com>
2025-04-01 15:24:14 -0600

    Add files via upload

---
+++ palabra.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+int N,i,j;
+char palabra [30];
+
+ printf("introduce una palabra:");
+   scanf("%s",palabra);
+ N = strlen(palabra); 
+ for ( int i= 0; i < N; i++);{
+ for ( int j =0; j < N; j++);{
+ }
+ printf("*");
+ }
+ printf("\n");
+    return 0;
+}
