3186e79b312cce2326ec2451d54753e336112866
abril200009 <ap2405118@gmail.com>
2025-05-08 13:21:18 -0600

    Add files via upload

---
+++ apuntador.cpp
@@ -0,0 +1,8 @@
+
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("La dirección de X es: %d\n", &x);
+    return 0;
+}
---
+++ punter0.cpp
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main() {
+    int x=17,y;
+    int * p;
+    p = &x;
+    printf("El valor de x es %d",*p);
+    y=*p+3;
+    printf("El valor de %d",y);
+    return 0;
+}
